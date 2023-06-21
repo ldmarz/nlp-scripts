e00c121795a2c089aae2df36e715c32b2e43610a
ldmarz <lenin.martinez@mercadolibre.cl>
2023-06-21 16:34:50 -0400

    adding loading bar

--- src/zero_shot_classifier.py
+++ src/zero_shot_classifier.py
@@ -1,5 +1,6 @@
 import csv
 import os
+from tqdm import tqdm
 from transformers import pipeline
 
 # Setup
@@ -10,21 +11,37 @@ candidate_labels = ["Product Availability", "Service Availability", "Financing",
 
 def classify_sequence(sequence, threshold=60):
     result = classifier(sequence, candidate_labels, multi_label=True)
-    return zip(*[(label, score * 100) for label, score in zip(result["labels"], result["scores"]) if score * 100 >= threshold])
+    scores_scaled = [score * 100 for score in result["scores"]]
+    labels_scores = list(zip(result["labels"], scores_scaled))
+
+    filtered_labels_scores = [(label, score) for label, score in labels_scores if score >= threshold]
+
+    if filtered_labels_scores:
+        labels, scores = zip(*filtered_labels_scores)
+    else:
+        labels, scores = [], []
+
+    return labels, scores
 
 def main():
-    with open("../input/text_to_analize.csv", "r") as input_file, open("../output/sentiment_analysis.csv", "w", newline="") as output_file:
-        reader = csv.reader(input_file)
+    with open("../input/text_to_analize.csv", "r") as input_file, open("../output/sentiment_analysis.csv", "w",
+                                                                       newline="") as output_file:
+        reader = list(csv.reader(input_file))
         writer = csv.writer(output_file)
 
-        header = next(reader)
+        header = reader.pop(0)
         header.extend(["Predicted Label", "Score (%)"])
         writer.writerow(header)
 
-        for row in reader:
-            predicted_labels, scores = classify_sequence(row[3])
-            row.extend([",".join(predicted_labels), ",".join(map(str, scores))])
-            writer.writerow(row)
+        total_rows = len(reader)
+        with tqdm(total=total_rows, desc="Processing rows", dynamic_ncols=True,
+                  bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}') as pbar:
+            for row in reader:
+                predicted_labels, scores = classify_sequence(row[3])
+                row.extend([",".join(predicted_labels), ",".join(map(str, scores))])
+                writer.writerow(row)
+                pbar.set_postfix_str(f"Processing row: {pbar.n + 1}, Label: {','.join(predicted_labels)}", refresh=True)
+                pbar.update()
 
 if __name__ == "__main__":
     main()
