6acd61b81734b2968ad0282f5ef3a354e55992f5
ldmarz <lenin.martinez@mercadolibre.cl>
2023-06-21 22:47:58 -0400

    refactoring to print every label

--- src/zero_shot_classifier.py
+++ src/zero_shot_classifier.py
@@ -6,41 +6,48 @@ from transformers import pipeline
 # Setup
 os.environ["TRANSFORMERS_CACHE"] = "./models"
 classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-candidate_labels = ["Product Availability", "Service Availability", "Financing",
-                    "Viewing/Visiting", "Price", "Miscellaneous/Other", "WhatsApp"]
+candidate_labels = ["Product Availability", "Financing",
+                    "Viewing/Visiting", "Price", "Miscellaneous/Other", "Whatsapp/Wap", "Communicating", "Video"]
 
-def classify_sequence(sequence, threshold=60):
+def classify_sequence(sequence, threshold=50):
+    # classify the sequence using the model
     result = classifier(sequence, candidate_labels, multi_label=True)
-    scores_scaled = [score * 100 for score in result["scores"]]
-    labels_scores = list(zip(result["labels"], scores_scaled))
 
-    filtered_labels_scores = [(label, score) for label, score in labels_scores if score >= threshold]
+    # create a dictionary mapping labels to their scores
+    labels = result["labels"]
+    scores = [score * 100 for score in result["scores"]]
+    labels_scores = dict(zip(labels, scores))
 
-    if filtered_labels_scores:
-        labels, scores = zip(*filtered_labels_scores)
-    else:
-        labels, scores = [], []
+    # create a new dictionary, where scores below the threshold are set to 0
+    for label, score in labels_scores.items():
+        if score < threshold:
+            labels_scores[label] = 0
 
-    return labels, scores
+    return labels_scores
 
 def main():
+    with open("../input/text_to_analize.csv", "r") as input_file:
+        reader = list(csv.reader(input_file))
+        total_rows = len(reader)
+
     with open("../input/text_to_analize.csv", "r") as input_file, open("../output/sentiment_analysis.csv", "w",
                                                                        newline="") as output_file:
-        reader = list(csv.reader(input_file))
+        reader = csv.reader(input_file)
         writer = csv.writer(output_file)
 
-        header = reader.pop(0)
-        header.extend(["Predicted Label", "Score (%)"])
+        header = next(reader)
+        header.extend(candidate_labels)
         writer.writerow(header)
 
-        total_rows = len(reader)
         with tqdm(total=total_rows, desc="Processing rows", dynamic_ncols=True,
-                  bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}') as pbar:
+                  bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}]') as pbar:
             for row in reader:
-                predicted_labels, scores = classify_sequence(row[3])
-                row.extend([",".join(predicted_labels), ",".join(map(str, scores))])
+                labels_scores = classify_sequence(row[3])
+
+                row.extend([labels_scores.get(label, 0) for label in candidate_labels])
                 writer.writerow(row)
-                pbar.set_postfix_str(f"Processing row: {pbar.n + 1}, Label: {','.join(predicted_labels)}", refresh=True)
+                pbar.set_postfix_str(f"Processing row: {pbar.n + 1}, Labels: {','.join(labels_scores.keys())}",
+                                     refresh=True)
                 pbar.update()
 
 if __name__ == "__main__":
